fddb6696dbc55be7bd643f4b9b777f0b09a4bb28
anranlee99 <anranlee99@gmail.com>
2024-06-03 14:22:35 -0700

    update qemu-plumbing

--- qemu-plumbing.js
+++ qemu-plumbing.js
@@ -56,6 +56,63 @@ Events:
         eventTarget.dispatchEvent({ type: "main-loop" });
     });
 
+    const main_loop_wait = DebugSymbol.fromName("main_loop_wait").address;
+    const memory = DebugSymbol.fromName("qemu_ram_block_by_name").address;
+
+    const qemu_ram_block_by_name_native = new NativeFunction(memory, "pointer", ["pointer"]);
+
+    const host_pointer_offset = 3 * 8;
+    const max_length_pointer_offset = host_pointer_offset + 8 * 4;
+
+    let printed = false;
+
+    Interceptor.attach(main_loop_wait, () => {
+        if (!printed) {
+            printed = true;
+
+            let res = qemu_ram_block_by_name_native(Memory.allocUtf8String("pc.ram"));
+
+            let host_pointer_address = res.add(host_pointer_offset);
+            let host_pointer_value = host_pointer_address.readPointer();
+
+            let max_length_pointer_address = res.add(max_length_pointer_offset);
+            let max_length_pointer_value = max_length_pointer_address.readPointer();
+            let size = max_length_pointer_value.toInt32();
+            Interceptor.attach(main_loop_wait, function () {
+                eventTarget.dispatchEvent({ type: "main-loop" });
+            });
+
+            Process.setExceptionHandler(function (details) {
+                if (details.type === "access-violation") {
+                    if (details.memory.operation === "read") {
+                        const addr = details.memory.address;
+                        Memory.protect(addr, 1, "rw-");
+
+                        eventTarget.dispatchEvent({
+                            type: "read-block",
+                            device: "memory",
+                            index: Math.floor(addr.sub(host_pointer_value).toInt32() / Process.pageSize),
+                        });
+                        return true;
+                    }
+                    if (details.memory.operation === "write") {
+                        const addr = details.memory.address;
+                        Memory.protect(addr, 1, "rw-");
+                        eventTarget.dispatchEvent({
+                            type: "write-block",
+                            device: "memory",
+                            index: Math.floor(addr.sub(host_pointer_value).toInt32() / Process.pageSize),
+                        });
+                        return true;
+                    }
+                }
+                return false;
+            });
+
+            Memory.protect(host_pointer_value, size, "---");
+        }
+    });
+
     globalThis.QuoteKit = {
         addEventListener: eventTarget.addEventListener.bind(eventTarget),
         removeEventListener: eventTarget.removeEventListener.bind(eventTarget),
