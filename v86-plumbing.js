a9a2409b8f7b20f62f62d05a38fd78bbc1e57853
JuiceBox7 <juiceinbox7@gmail.com>
2024-06-07 19:37:01 -0700

    updating v86 plumbing and adding test for reference

--- v86-plumbing.js
+++ v86-plumbing.js
@@ -2,27 +2,46 @@
 This API is meant to be implemented in an environment like the browser where the code is executed inside the same process that hosts the actual emulator and events as dispatched synchronously (e.g. the state is not changing during the saving of a residue).
 
 Methods:
-    getDeviceInfo() -> Empty obj
-    setEmulator(v86Emulator)
-    mainLoop()
+    getDeviceInfo() -> obj containing name, block size, and block count for devices (currently does nothing)
     saveResidue() -> ArrayBuffer
-    residueLoop()
+    restoreResidue(buffer)
+    readBlock(device,index) -> ArrayBuffer
+    writeBlock(device,index,buffer)
+    resetBlockNotifications()
+    applyStimulus(obj)
 Events:
-    None currently
+    main-loop: details nothing
+    frame: visual animation frame, details pixels
+    read-block: details device id and block index
+    write-block: details device id and block index
+    stimulus: details an environmental stimulus object
 
 */
 
 (function () {
   "use strict";
   const eventTarget = new EventTarget();
+
   let emulator;
+
+  const DISK_PAGE_SIZE = 1024 * 4;
+
+  const allowedDiskPages = new Set();
+
+  const original_create_memory = CPU.prototype.create_memory;
 
   function getDeviceInfo() {
     return {};
   }
 
-  function setEmulator(v86Emulator) {
+  function setup(v86Emulator) {
     emulator = v86Emulator;
+    CPU.prototype.codegen_finalize = function () {}; // block jit
+    const old_get = emulator.disk_images.hda.get;
+    emulator.disk_images.hda.get = function get(start, len, fn) {
+      disk_get(start, len);
+      return old_get.bind(this)(start, len, fn);
+    }
   }
 
   function mainLoop() {
@@ -94,13 +113,37 @@ Events:
     emulator.v86.do_tick = new_do_tick;
   }
 
+  function disk_get(start, len) {
+    const pageStart = (start / DISK_PAGE_SIZE) | 0;
+    for (let i = 0; i < len; i += DISK_PAGE_SIZE) {
+      const page = pageStart + i / DISK_PAGE_SIZE;
+      if (!allowedDiskPages.has(page)) {
+        allowedDiskPages.add(page);
+        console.log("disk get: ", page);
+      }
+    }
+  }
+
+  CPU.prototype.create_memory = function (size) {
+    const original_allocate_memory = this.allocate_memory;
+    this.allocate_memory = function (size) {
+      const ptr = original_allocate_memory(size);
+      pmemStart = ptr;
+      pmemLength = size;
+      console.log("buffer found");
+      return ptr;
+    };
+    original_create_memory.bind(this)(size);
+  };
+
   globalThis.QuoteKit = {
     addEventListener: eventTarget.addEventListener.bind(eventTarget),
     removeEventListener: eventTarget.removeEventListener.bind(eventTarget),
     getDeviceInfo,
-    setEmulator,
+    setup,
     mainLoop,
     saveResidue,
     residueLoop,
+    disk_get,
   };
 })();
